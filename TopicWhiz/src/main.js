536ff4701ebdb1ab8efdab391177b49e6395f3d9
David <15Dkatz@users.noreply.github.com>
2016-08-13 16:23:06 -0700

    Added the componentDidUpdate() life-cycle component.
    This allows you to avoid the asynchronous nature of setState and setStorage. This way you don't the storage of the previous state but always the updated and current state.

--- TopicWhiz/src/main.js
+++ TopicWhiz/src/main.js
@@ -1,47 +1,162 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  Navigator
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  AsyncStorage
 } from 'react-native';
 
-import signIn from './components/auth/signIn';
-import signUp from './components/auth/signUp';
-import chooseName from './components/auth/chooseName';
-import forgotPassword from './components/auth/forgot-password';
-
-import topics from './components/topics';
-import topicDetail from './components/topic-detail';
-
-const routes = {
-  signIn,
-  signUp,
-  chooseName,
-  forgotPassword,
-  topics,
-  topicDetail,
-}
-
 module.exports = React.createClass({
-  render() {
+  getInitialState() {
+    return ({
+      tasks: [],
+      completedTasks: [],
+      task: ''
+    })
+  },
+
+  componentWillMount() {
+    AsyncStorage.getItem('tasks')
+      .then((response) => {
+        this.setState({tasks: JSON.parse(response)})
+      });
+    AsyncStorage.getItem('completedTasks')
+      .then((response) => {
+        this.setState({completedTasks: JSON.parse(response)})
+      });
+  },
+
+  setStorage() {
+    AsyncStorage.setItem('tasks', JSON.stringify(this.state.tasks));
+    AsyncStorage.setItem('completedTasks', JSON.stringify(this.state.completedTasks));
+  },
+
+  componentDidUpdate() {
+    this.setStorage();
+  },
+
+  renderList(tasks) {
+    // return individual Views or rows based on the argued tasks
     return (
-      <Navigator
-        initialRoute={{name: 'signIn'}}
-        renderScene={this.renderScene}
-      />
+      tasks.map((task, index) => {
+        return (
+          <View key={task} style={styles.task}>
+            <Text>
+              {task}
+            </Text>
+            <TouchableOpacity
+              onPress={()=>this.completeTask(index)}
+            >
+              <Text>
+                &#10003;
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )
+      })
     )
   },
 
-  renderScene(route, navigator) {
-    let Component = routes[route.name];
-    let {displayName, title, author, row_uid} = route;
-
+  renderCompleted(tasks) {
     return (
-      <Component
-        navigator={navigator}
-        displayName={displayName}
-        title={title}
-        author={author}
-        row_uid={row_uid}
-      />
+      tasks.map((task, index) => {
+        return (
+          <View key={task} style={styles.task}>
+            <Text style={styles.completed}>
+              {task}
+            </Text>
+            <TouchableOpacity
+              onPress={()=>this.deleteTask(index)}
+            >
+              <Text>
+                &#10005;
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )
+      })
     )
+  },
+
+  deleteTask(index) {
+    let completedTasks = this.state.completedTasks;
+    completedTasks = completedTasks.slice(0, index).concat(completedTasks.slice(index+1));
+    this.setState({completedTasks});
+  },
+
+  completeTask(index) {
+    let tasks = this.state.tasks;
+    tasks = tasks.slice(0, index).concat(tasks.slice(index+1));
+
+    let completedTasks = this.state.completedTasks;
+    completedTasks = completedTasks.concat([this.state.tasks[index]]);
+
+    this.setState({
+      tasks,
+      completedTasks
+    });
+  },
+
+  addTask() {
+      let tasks = this.state.tasks.concat([this.state.task]);
+      this.setState({tasks});
+  },
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          To-Do Master
+        </Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Add a task...'
+          onChangeText={(text) => {
+            this.setState({task: text});
+          }}
+          onEndEditing={() => this.addTask()}
+        />
+        <ScrollView>
+          {this.renderList(this.state.tasks)}
+          {this.renderCompleted(this.state.completedTasks)}
+        </ScrollView>
+      </View>
+    )
+  }
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    margin: 30,
+    marginTop: 40,
+    textAlign: 'center',
+    fontSize: 18
+  },
+  task: {
+    flexDirection: 'row',
+    height: 60,
+    borderBottomWidth: 1,
+    borderColor: 'black',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20
+  },
+  input: {
+    height: 60,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'black',
+    textAlign: 'center',
+    margin: 10
+  },
+  completed: {
+    color: '#555',
+    textDecorationLine: 'line-through'
   }
 })
